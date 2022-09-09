cc3e2d4aced876e9ad1f34cbe34354991523453d
Dasilvadavid <57997901+dasilvadav07@users.noreply.github.com>
2022-09-09 16:47:19 +0200

    first commit

---
+++ src/main/java/com/findUrPlants/fuPApi/FuPApiApplication.java
@@ -0,0 +1,13 @@
+package com.findUrPlants.fuPApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuPApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuPApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findUrPlants/fuPApi/FuPApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findUrPlants.fuPApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuPApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
